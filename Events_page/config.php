dacda9dca478a510586b990f934a0564b791ffb9
aaronkwk <aaron.koh.2024@computing.smu.edu.sg>
2025-11-07 05:00:46 +0800

    bye chat

--- Events_page/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Sendbird credentials (keep private!)
-define('SENDBIRD_APP_ID', 'A3184D08-9158-4B73-9670-3D1DEFB72FDD');
-define('SENDBIRD_API_TOKEN', '2600d97d6629420665eb343fe88f8f56387c0154');
-define('SENDBIRD_API_HOST', 'https://api-A3184D08-9158-4B73-9670-3D1DEFB72FDD.sendbird.com');
-?>
