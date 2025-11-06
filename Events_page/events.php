db8144dc27c9ad91bc71bd2d321a9161a5f49c86
akhil-ad8 <akhileshdanapalan@gmail.com>
2025-11-06 21:42:50 +0800

    removed "testing", add event works now

--- Events_page/events.php
+++ Events_page/events.php
@@ -61,7 +61,7 @@ if (!isset($_SESSION['username'])){
   <!-- Featured Event Banner -->
 
   <div class="text-center mb-4">
-    <h2 class="fw-bold" style="color:#041373;">Discover Events at SMU Testing</h2>
+    <h2 class="fw-bold" style="color:#041373;">Discover Events at SMU</h2>
     <p class="text-muted mb-0"><strong>Explore events in the school - Find events you like and save them to My Events.</strong></p>
   </div>
 
